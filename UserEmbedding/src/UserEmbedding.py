7604ed345b6be6f62565b5d9659f9bd6f72d653c
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2025-10-27 11:27:56 +0700

    Update UserEmbedding.py

--- UserEmbedding/src/UserEmbedding.py
+++ UserEmbedding/src/UserEmbedding.py
@@ -17,6 +17,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
+from accelerate import Accelerator, DistributedDataParallelKwargs
+from accelerate.state import AcceleratorState
+
 from data.dataset import DanceDataset
 from src.backbones import DSTformer
 
@@ -26,19 +29,24 @@ class UserEmbedding:
         # self.motionbert_backbone = DSTformer(dim_in=3, dim_out=3, dim_feat=args.dim_feat, dim_rep=args.dim_rep,
         #                         depth=args.depth, num_heads=args.num_heads, mlp_ratio=args.mlp_ratio, norm_layer=partial(nn.LayerNorm, eps=1e-6),
         #                         maxlen=args.maxlen, num_joints=args.num_joints)
-
+        
+        ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+        self.accelerator = Accelerator(kwargs_handlers=[ddp_kwargs])
+        state = AcceleratorState()
+        num_processes = state.num_processes
+        
+        self.accelerator.wait_for_everyone()
+        
         checkpoint = None
         if checkpoint_path != "":
             checkpoint = torch.load(
                 checkpoint_path, map_location=self.accelerator.device, weights_only=False
             )
+            
+    def prepare(self, objects):
+        return self.accelerator.prepare(*objects)
 
     def train(self, args):
-        load_loop = (
-            partial(tqdm, position=1, desc="Batch")
-            if self.accelerator.is_main_process
-            else lambda x: x
-        )
 
         train_dataset = DanceDataset(
             data_path=args.data_path,
@@ -57,6 +65,16 @@ class UserEmbedding:
             pin_memory=True,
             drop_last=True,
         )
+        
+        train_data_loader = self.accelerator.prepare(train_data_loader)
+        
+        load_loop = (
+            partial(tqdm, position=1, desc="Batch")
+            if self.accelerator.is_main_process
+            else lambda x: x
+        )
+        
+        self.accelerator.wait_for_everyone()
 
         for video, pose_est in load_loop(train_data_loader):
             print(video.shape, pose_est.shape)
