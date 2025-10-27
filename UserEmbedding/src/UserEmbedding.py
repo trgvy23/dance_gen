fb554478ff6548dd1d06590fed933e234a689f66
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2025-10-27 12:36:54 +0700

    Update UserEmbedding.py

--- UserEmbedding/src/UserEmbedding.py
+++ UserEmbedding/src/UserEmbedding.py
@@ -23,20 +23,30 @@ from accelerate.state import AcceleratorState
 from data.dataset import DanceDataset
 from src.backbones import DSTformer
 
+
 class UserEmbedding:
     # TODO: build input pipeline for MotionBERT
     def __init__(self, args, checkpoint_path=""):
         # self.motionbert_backbone = DSTformer(dim_in=3, dim_out=3, dim_feat=args.dim_feat, dim_rep=args.dim_rep,
         #                         depth=args.depth, num_heads=args.num_heads, mlp_ratio=args.mlp_ratio, norm_layer=partial(nn.LayerNorm, eps=1e-6),
         #                         maxlen=args.maxlen, num_joints=args.num_joints)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        
+        ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+        self.accelerator = Accelerator(kwargs_handlers=[ddp_kwargs])
+        state = AcceleratorState()
+        num_processes = state.num_processes
+
+        self.accelerator.wait_for_everyone()
+
         checkpoint = None
         if checkpoint_path != "":
             checkpoint = torch.load(
-                checkpoint_path, map_location=self.device, weights_only=False
+                checkpoint_path,
+                map_location=self.accelerator.device,
+                weights_only=False,
             )
-            
+
+    def prepare(self, objects):
+        return self.accelerator.prepare(*objects)
 
     def train(self, args):
         print("Loading DanceDataset...")
@@ -49,7 +59,7 @@ class UserEmbedding:
         )
 
         num_cpus = multiprocessing.cpu_count()
-        
+
         print("Creating data loaders...")
         train_data_loader = DataLoader(
             train_dataset,
@@ -60,6 +70,16 @@ class UserEmbedding:
             drop_last=True,
         )
 
-        for video, pose_est in tqdm(train_data_loader, desc="Batch"):
+        train_data_loader = self.accelerator.prepare(train_data_loader)
+
+        load_loop = (
+            partial(tqdm, position=1, desc="Batch")
+            if self.accelerator.is_main_process
+            else lambda x: x
+        )
+
+        self.accelerator.wait_for_everyone()
+
+        for video, pose_est in enumerate(load_loop(train_data_loader)):
             print(video.shape, pose_est.shape)
             break
