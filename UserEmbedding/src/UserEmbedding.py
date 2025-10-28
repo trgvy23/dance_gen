addb654aab5069dae2a36f3c96e22804d375b0c0
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2025-10-29 00:28:29 +0700

    Update UserEmbedding.py

--- UserEmbedding/src/UserEmbedding.py
+++ UserEmbedding/src/UserEmbedding.py
@@ -82,19 +82,19 @@ class UserEmbedding:
             )
 
         # TODO: hardcode args for MotionBERT for now: https://github.com/Walter0807/MotionBERT/blob/main/configs/pose3d/MB_ft_h36m_global_lite.yaml
-        # self.motionbert_backbone = DSTformer(dim_in=3, dim_out=3, dim_feat=256, dim_rep=512,
-        #                         depth=5, num_heads=8, mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6),
-        #                         maxlen=243, num_joints=17)
-        # if torch.cuda.is_available():
-        #     self.motionbert_backbone = nn.DataParallel(self.motionbert_backbone)
-        #     self.motionbert_backbone = self.motionbert_backbone.cuda()
+        self.motionbert_backbone = DSTformer(dim_in=3, dim_out=3, dim_feat=256, dim_rep=512,
+                                depth=5, num_heads=8, mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6),
+                                maxlen=243, num_joints=17)
+        if torch.cuda.is_available():
+            self.motionbert_backbone = nn.DataParallel(self.motionbert_backbone)
+            self.motionbert_backbone = self.motionbert_backbone.cuda()
             
         # print('Loading checkpoint', args.motionbert_checkpoint)
         # checkpoint = torch.load(args.motionbert_checkpoint, map_location=lambda storage, loc: storage)
         # self.motionbert_backbone.load_state_dict(checkpoint['model_pos'], strict=True)
         
         
-        # self.optimizer = optim.Adam(model.parameters(), lr=0.01)
+        self.optimizer = optim.Adam(self.motionbert_backbone.parameters(), lr=0.0005, weight_decay=0.01)
         # self.num_epochs = 1
 
         ############### Metric Learning ###############
@@ -221,33 +221,25 @@ class UserEmbedding:
             gerne_label = gerne_label.to(self.accelerator.device)
             dancer_label = dancer_label.to(self.accelerator.device)
 
-            print("Batch idx:", batch_idx)
-            print("Video shape:", video.shape)
-            print("Pose est shape:", pose_est.shape)
-            print("Gerne labels:", gerne_label)
-            print("Dancer labels:", dancer_label)
-
-            break  # TODO: Remove this break after implementing the training loop
-
             # TODO: forward pass
-            # embeddings = self.motionbert_backbon(video)  # Compute embeddings using the model
-            # embeddings = F.normalize(embeddings, p=2, dim=1)
+            embeddings = self.motionbert_backbon(video)  # Compute embeddings using the model
+            embeddings = F.normalize(embeddings, p=2, dim=1)
 
-            # triplets_d = self.dancer_miner(embeddings, dancer_labels)
-            # triplets_g = self.gerne_miner(embeddings, gerne_labels)
+            triplets_d = self.dancer_miner(embeddings, dancer_label)
+            triplets_g = self.gerne_miner(embeddings, gerne_label)
 
-            # loss_dancer = self.dancer_loss_func(embeddings, dancer_label, indices_tuple=triplets_d)
-            # loss_gerne = self.genre_loss_func(embeddings, genre_label, indices_tuple=triplets_g)
-            # loss = self.lambda_dancer * loss_dancer + self.lambda_genre * loss_gerne
+            loss_dancer = self.dancer_loss_func(embeddings, dancer_label, indices_tuple=triplets_d)
+            loss_gerne = self.genre_loss_func(embeddings, gerne_label, indices_tuple=triplets_g)
+            loss = self.lambda_dancer * loss_dancer + self.lambda_genre * loss_gerne
 
-            # if self.use_triplet_reg:
-            #     T1, T2 = build_hierarchical_triplets(dancer_label, genre_label)  # see helper below
-            #     L1 = self.triplet_reg(embeddings, dancer_label, indices_tuple=T1)  # same-dancer vs same-genre-diff-dancer
-            #     L2 = self.triplet_reg(embeddings, genre_label,  indices_tuple=T2)  # same-genre-diff-dancer vs diff-genre
-            #     loss = loss + self.mu_triplet * (L1 + 0.5 * L2)
+            if self.use_triplet_reg:
+                T1, T2 = build_hierarchical_triplets(dancer_label, gerne_label)  # see helper below
+                L1 = self.triplet_reg(embeddings, dancer_label, indices_tuple=T1)  # same-dancer vs same-genre-diff-dancer
+                L2 = self.triplet_reg(embeddings, gerne_label,  indices_tuple=T2)  # same-genre-diff-dancer vs diff-genre
+                loss = loss + self.mu_triplet * (L1 + 0.5 * L2)
 
-            # self.optimizer.zero_grad()
-            # self.accelerator.backward(loss)
-            # self.optimizer.step()
-            # if self.accelerator.is_main_process and (batch_idx % 20 == 0):
-            #     print(f"[{batch_idx}] L_dancer={Ld.item():.4f}  L_genre={Lg.item():.4f}  total={loss.item():.4f}")
+            self.optimizer.zero_grad()
+            self.accelerator.backward(loss)
+            self.optimizer.step()
+            if self.accelerator.is_main_process and (batch_idx % 20 == 0):
+                print(f"[{batch_idx}] L_dancer={loss_dancer.item():.4f}  L_genre={loss_gerne.item():.4f}  total={loss.item():.4f}")
