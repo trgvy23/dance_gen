1b452450a8087dae497b5c5596c02c064d4d76ad
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2025-11-04 23:01:36 +0700

    TT

--- UserEmbedding/train.py
+++ UserEmbedding/train.py
@@ -1,11 +1,6 @@
 from args import parse_train_opt
 from src.UserEmbedding import UserEmbedding
 
-import os
-
-# Make JAX stay on CPU and not preallocate GPU memory
-os.environ["JAX_PLATFORMS"] = "cpu"        # new-style
-os.environ["JAX_PLATFORM_NAME"] = "cpu"  
 
 def train(args):
     model = UserEmbedding(args.checkpoint)
