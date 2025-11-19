8d7c8dbbb8527721e74d1d41f0ba0974d015f9c5
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2025-11-20 01:17:50 +0700

    Update UserEmbedding.py

--- UserEmbedding/src/UserEmbedding.py
+++ UserEmbedding/src/UserEmbedding.py
@@ -34,6 +34,19 @@ from src.models import UserEmbeddingNet
 from src.backbone import MotionBERTBackbone
 
 
+class MyAccuracyCalculator(accuracy_calculator.AccuracyCalculator):
+    def calculate_precision_at_3(self, knn_labels, query_labels, **kwargs):
+        # query_labels is shape [N], precision_at_k expects [N, 1]
+        return accuracy_calculator.precision_at_k(knn_labels, query_labels[:, None], 3)
+
+    def calculate_precision_at_5(self, knn_labels, query_labels, **kwargs):
+        return accuracy_calculator.precision_at_k(knn_labels, query_labels[:, None], 5)
+
+    def requires_knn(self):
+        # tell the calculator that these metrics need kNN results
+        return super().requires_knn() + ["precision_at_3", "precision_at_5"]
+
+
 class UserEmbedding:
     def __init__(self, args):
         self.hparams = JsonConfig(args.hparams)
@@ -292,8 +305,19 @@ class UserEmbedding:
         train_dancer_np = train_dancer.cpu().numpy()
         test_dancer_np = test_dancer.cpu().numpy()
 
-        acc_calc = accuracy_calculator.AccuracyCalculator(
-            include=("precision_at_1", "r_precision", "mean_average_precision_at_r"),
+        # acc_calc = accuracy_calculator.AccuracyCalculator(
+        #     include=("precision_at_1", "r_precision", "mean_average_precision_at_r"),
+        #     device=self.accelerator.device,
+        # )
+
+        acc_calc = MyAccuracyCalculator(
+            include=(
+                "precision_at_1",
+                "precision_at_3",
+                "precision_at_5",
+                "r_precision",
+                "mean_average_precision_at_r",
+            ),
             device=self.accelerator.device,
         )
 
@@ -306,9 +330,21 @@ class UserEmbedding:
             ref_includes_query=False,
         )
 
+        # print(
+        #     "[Eval] DANCER  | p@1: {:.4f}, R-prec: {:.4f}, mAP@R: {:.4f}".format(
+        #         dancer_acc["precision_at_1"],
+        #         dancer_acc["r_precision"],
+        #         dancer_acc["mean_average_precision_at_r"],
+        #     )
+        # )
+
         print(
-            "[Eval] DANCER  | p@1: {:.4f}, R-prec: {:.4f}, mAP@R: {:.4f}".format(
+            "[Eval] DANCER  | "
+            "p@1: {:.4f}, p@3: {:.4f}, p@5: {:.4f}, "
+            "R-prec: {:.4f}, mAP@R: {:.4f}".format(
                 dancer_acc["precision_at_1"],
+                dancer_acc["precision_at_3"],
+                dancer_acc["precision_at_5"],
                 dancer_acc["r_precision"],
                 dancer_acc["mean_average_precision_at_r"],
             )
