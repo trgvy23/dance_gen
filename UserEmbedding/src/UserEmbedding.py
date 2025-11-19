036c24ec89209b90929020bfcfbd042262a3fde8
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2025-11-20 01:47:11 +0700

    Update UserEmbedding.py

--- UserEmbedding/src/UserEmbedding.py
+++ UserEmbedding/src/UserEmbedding.py
@@ -36,14 +36,26 @@ from src.backbone import MotionBERTBackbone
 
 class MyAccuracyCalculator(accuracy_calculator.AccuracyCalculator):
     def calculate_precision_at_3(self, knn_labels, query_labels, **kwargs):
-        # query_labels is shape [N], precision_at_k expects [N, 1]
-        return accuracy_calculator.precision_at_k(knn_labels, query_labels[:, None], 3)
+        return accuracy_calculator.precision_at_k(
+            knn_labels,
+            query_labels[:, None],
+            3,
+            self.avg_of_avgs,
+            self.return_per_class,
+            self.label_comparison_fn,
+        )
 
     def calculate_precision_at_5(self, knn_labels, query_labels, **kwargs):
-        return accuracy_calculator.precision_at_k(knn_labels, query_labels[:, None], 5)
+        return accuracy_calculator.precision_at_k(
+            knn_labels,
+            query_labels[:, None],
+            5,
+            self.avg_of_avgs,
+            self.return_per_class,
+            self.label_comparison_fn,
+        )
 
     def requires_knn(self):
-        # tell the calculator that these metrics need kNN results
         return super().requires_knn() + ["precision_at_3", "precision_at_5"]
 
 
